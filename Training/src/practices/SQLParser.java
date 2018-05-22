1c2e551777b1574e9ddb4ff0aeb3d46439959a74
Prashant <prashant2serve4u@gmail.com>
2018-05-22 23:24:23 +0530

    Update SQLparser code

--- Training/src/practices/SQLParser.java
+++ Training/src/practices/SQLParser.java
@@ -5,50 +5,48 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class SQLParser {
-	
-	public static void main(String [] args) throws IOException
-	{
-		
-		int noofrows=0,noofconditions=0;
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		String input=br.readLine();
-		//System.out.println(input.length());
-		String [] splitstring=input.split(",");
-		for(int i=0;i<splitstring.length;i++)
-		{
-			if(i==0)
-			{
-			noofrows=Integer.parseInt(splitstring[i]);
-			}
-			else
-			{
-				noofconditions=	Integer.parseInt(splitstring[i]);
+
+	public static void main(String[] args) throws IOException {
+
+		int noofrows = 0, noofconditions = 0;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		// System.out.println(input.length());
+		String[] splitstring = input.split(",");
+		for (int i = 0; i < splitstring.length; i++) {
+			if (i == 0) {
+				noofrows = Integer.parseInt(splitstring[i]);
+			} else {
+				noofconditions = Integer.parseInt(splitstring[i]);
 			}
 		}
-		
-		//System.out.println(noofrows+","+noofconditions);
-	//String [] header=new String[noofrows];
-		String input1=br.readLine();
-		//System.out.println(input1.length());
-		String input2=null;
-		String [] header=input1.split(",");
-		String [][]rows = null;
-		for(int i=0;i<noofrows;i++)
+
+		// System.out.println(noofrows+","+noofconditions);
+		// String [] header=new String[noofrows];
+		String input1 = br.readLine();
+		// System.out.println(input1.length());
+		String input2 = null;
+		String[] header = input1.split(",");
+		String[][] rows = new String[noofrows][header.length];
+		for (int i = 0; i < noofrows; i++) 
 		{
-		input2=br.readLine();
-		String sliparr[]=input2.split(",");
-			for(int ii=0;ii<sliparr.length;ii++)
+			input2 = br.readLine();
+			String sliparr[] = input2.split(",");
+			System.out.println(sliparr.length);
+			for (int ii = 0; ii < sliparr.length; ii++) 
 			{
-				rows[i][ii]=sliparr[ii];
+				rows[i][ii] = sliparr[ii];
 			}
 		}
-	String conditions[]=null;
-		for(int i=0;i<noofconditions;i++)
+		String conditions[] = new String[noofconditions];
+		for (int i = 0; i < noofconditions; i++) 
 		{
-			String input3=br.readLine();
-			
+			conditions[i] = br.readLine();
+
 		}
+		HashMap<String,String> condition=new HashMap();
 	}
 }
